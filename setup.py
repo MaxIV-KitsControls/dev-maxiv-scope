3aae22a573eb449bf487a2ff2864b0951ac996a0
Paul Bell <paubel@ec-kitslab-0.maxlab.lu.se>
2014-03-05 15:48:40 +0100

    fix packaging

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from distutils.core import setup
 setup(name = 'tangods-scope',
       version = '0.1',
       description = 'Tango device for Rohde and Schwarz RTO 1004 oscilloscope',
+      packages = ['RohdeSchwarzRTO']
       author='Paul Bell',
       scripts = ['scripts/Scope'],
-      package_dir = {'' : 'src'},
-     )
+      package_dir = {'RohdeSchwarzRTO' : 'src'},
+      )
