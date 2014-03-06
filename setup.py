b565e706a0fdda9249b0d5e2a3d75627934825aa
Paul Bell <paubel@ec-kitslab-0.maxlab.lu.se>
2014-03-06 13:22:31 +0100

    package version 1

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 from distutils.core import setup
 setup(name = 'tangods-scope',
-      version = '0.1',
+      version = '1.0',
       description = 'Tango device for Rohde and Schwarz RTO 1004 oscilloscope',
       package_dir = {'RohdeSchwarzRTO':'src'},
       packages = ['RohdeSchwarzRTO'],
       author='Paul Bell',
-      scripts = ['scripts/Scope'],
+      scripts = ['scripts/RohdeSchwarzRTO'],
       )
