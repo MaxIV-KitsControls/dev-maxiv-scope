9ed418ad09c01b1ad7662a965fa80c58a0f0c3ca
Johan Forsberg <johan.forsberg@maxlab.lu.se>
2014-05-06 12:17:57 +0200

    v 2.3.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from distutils.core import setup
 setup(name = 'tangods-scope',
-      version = '2.3.0',
+      version = '2.3.1',
       description = 'Tango device for Rohde and Schwarz RTO 1004 oscilloscope',
       package_dir = {'RohdeSchwarzRTO':'src'},
       packages = ['RohdeSchwarzRTO'],
