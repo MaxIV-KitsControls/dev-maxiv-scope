8ee9f18e55b847a00a80ce55fd8f64fbbfa9dc5b
Vincent Michel <vincent.michel@maxlab.lu.se>
2015-03-09 16:52:35 +0100

    bump version 3.1.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def safe_read(fname):
 
 # Setup
 setup(name="tangods-scope",
-      version="3.0.2",
+      version="3.1.0",
       description="Device servers for the Rohde and Schwarz oscilloscopes.",
       author="Vincent Michel; Paul Bell",
       author_email="vincent.michel@maxlab.lu.se; paul.bell@maxlab.lu.se",
