9dc6b4c5f0aed4fbb78890eb2b34c614582a97eb
Vincent Michel <vincent.michel@maxlab.lu.se>
2015-03-26 15:32:57 +0100

    move rtm and rto device class to their own module

--- scopedevice/__init__.py
+++ scopedevice/__init__.py
@@ -3,5 +3,7 @@
 __all__ = ['ScopeDevice', 'RTOScope', "RTMScope",
            'run_rto', 'run_rtm', 'run', 'RTO_NAME', 'RTM_NAME']
 
-from scopedevice.device import ScopeDevice, RTOScope, RTMScope
+from scopedevice.device import ScopeDevice
+from scopedevice.rtm import RTMScope
+from scopedevice.rto import RTOScope
 from scopedevice.server import RTO_NAME, RTM_NAME, run_rto, run_rtm, run
